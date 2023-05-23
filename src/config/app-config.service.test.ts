530a609cac6b6d8ca9d31fb7c8186a933bf6e0c8
abhishandy <abhishek.sandy@gmail.com>
2023-05-23 16:57:33 -0400

    fully cover app config

--- src/config/app-config.service.test.ts
+++ src/config/app-config.service.test.ts
@@ -23,6 +23,16 @@ describe('AppConfigService', () => {
         REDIS_PORT: 6379,
         REDIS_PASSWORD: 'password',
         HASHING_SECRET: 'secret',
+        STRIPE_WEBHOOK_SECRET: 'secret',
+        SMART_CONTRACT_PRIVATE_KEY: 'ethprv123',
+        SMART_CONTRACT_ADDRESS: 'eth123',
+        BLOCK_CHAIN_NODE_URI: 'http://localhost:8545',
+        SMTP_PASSWORD: 'password',
+        SMS_API_KEY: 'key',
+        MINIO_ENDPOINT: 'http://localhost:9000',
+        MINIO_PORT: 9000,
+        MINIO_ACCESS_KEY: 'key',
+        MINIO_SECRET_KEY: 'secret',
       }),
     );
   });
@@ -102,5 +112,45 @@ describe('AppConfigService', () => {
 
   it('should return the hashing secret', () => {
     expect(service.hashingSecret).toBeDefined();
+  });
+
+  it('should return the Stripe webhook secret', () => {
+    expect(service.stripeWebHookSecret).toBeDefined();
+  });
+
+  it('should return the Smart Contract private key', () => {
+    expect(service.smartContractPrivateKey).toBeDefined();
+  });
+
+  it('should return the Smart Contract address', () => {
+    expect(service.smartContractAddress).toBeDefined();
+  });
+
+  it('should return the blockchain node url', () => {
+    expect(service.blockChainNodeURI).toBeDefined();
+  });
+
+  it('should return the SMTP password', () => {
+    expect(service.smtpPassword).toBeDefined();
+  });
+
+  it('should return the SMS API key', () => {
+    expect(service.smsApiKey).toBeDefined();
+  });
+
+  it('should return the Minio endpoint', () => {
+    expect(service.objectStorageUrl).toBeDefined();
+  });
+
+  it('should return the Minio port', () => {
+    expect(service.objectStoragePort).toBeDefined();
+  });
+
+  it('should return the Minio access key', () => {
+    expect(service.objectStorageAccessKey).toBeDefined();
+  });
+
+  it('should return the Minio secret key', () => {
+    expect(service.objectStorageSecretKey).toBeDefined();
   });
 });
