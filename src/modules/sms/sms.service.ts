f0944d9aedc6ac7c7a452847ffe01c468e133bb0
abhishandy <abhishek.sandy@gmail.com>
2023-05-24 06:36:29 -0400

    test: sms service

--- src/modules/sms/sms.service.ts
+++ src/modules/sms/sms.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { IsCurrency } from 'class-validator';
 import { MessageBird, initClient } from 'messagebird';
-import { AppConfigService } from 'src/config/app-config.service';
+import { AppConfigService } from '../../config/app-config.service';
 
 @Injectable()
 export class SmsService {
@@ -80,7 +79,6 @@ export class SmsService {
   async sendTransactionVerificationTokenBySmsToAPatient(
     token: string,
     phoneNumber: string,
-    amount: number,
   ): Promise<void> {
     const params = {
       originator: 'WiiQare',
