a9836867dd7d260b8c03b43b3a806ae963bd5f91
lukebrichey <lukebrichey@gmail.com>
2023-06-19 13:56:06 -0400

    feat: Completed tests for mintVoucher and getVoucherById

--- src/modules/smart-contract/smart-contract.service.test.ts
+++ src/modules/smart-contract/smart-contract.service.test.ts
@@ -2,7 +2,8 @@ import { SmartContractService } from './smart-contract.service';
 import { AppConfigService } from '../../config/app-config.service';
 import Web3 from 'web3';
 import { MintVoucherDto } from './dto/mint-voucher.dto';
-import { VoucherStatus } from '../../common/constants/enums';
+import * as helpers from '../../helpers/common.helper';
+
 describe('SmartContractService', () => {
   // Mock services
   const mockAppConfigService = {
@@ -19,6 +20,17 @@ describe('SmartContractService', () => {
           mintVoucher: jest.fn().mockReturnValue({
             send: jest.fn().mockResolvedValue({
               transactionHash: '0x123',
+            }),
+          }),
+          vouchers: jest.fn().mockReturnValue({
+            call: jest.fn().mockResolvedValue({
+              owner: '0x123',
+              patient: '0x456',
+              amount: 1,
+              currency: 'USD',
+              status: 1,
+              createdAt: 1234567890,
+              updatedAt: 1234567890,
             }),
           }),
         },
@@ -70,14 +82,14 @@ describe('SmartContractService', () => {
   });
 
   describe('mintVoucher', () => {
-    it('should mint a voucher', async () => {
-      const mintVoucherDto: MintVoucherDto = {
-        amount: 1,
-        currency: 'USD',
-        ownerId: '0x123',
-        patientId: '0x456',
-      };
+    const mintVoucherDto: MintVoucherDto = {
+      amount: 1,
+      currency: 'USD',
+      ownerId: '0x123',
+      patientId: '0x456',
+    };
 
+    it('should mint a voucher', async () => {
       const gasFees = { gasPrice: 1, safeLow: 1, standard: 1, fast: 1 };
 
       smartContractService.getGasFees = jest.fn().mockResolvedValue(gasFees);
@@ -88,6 +100,72 @@ describe('SmartContractService', () => {
       expect(response).toEqual({
         transactionHash: '0x123',
       });
+    });
+
+    it('should throw an error if the contract method throws an error', async () => {
+      mockWeb3.eth.Contract = jest.fn().mockImplementation(() => {
+        return {
+          methods: {
+            mintVoucher: jest.fn().mockReturnValue({
+              send: jest.fn().mockRejectedValue(new Error('Contract Error')),
+            }),
+          },
+        };
+      });
+
+      smartContractService = new SmartContractService(
+        mockAppConfigService,
+        mockWeb3,
+      );
+
+      const logErrorSpy = jest.spyOn(helpers, 'logError');
+
+      await smartContractService.mintVoucher(mintVoucherDto);
+
+      expect(logErrorSpy).toHaveBeenCalledWith(
+        'Error in mintVoucher: Error: Contract Error',
+      );
+
+      logErrorSpy.mockRestore();
+    });
+  });
+
+  describe('getVoucherById', () => {
+    const voucherId = 'someVoucherId';
+
+    it('should return voucher information', async () => {
+      // The implementation of this method is still in progress, so this
+      // test is just a placeholder for now.
+      await smartContractService.getVoucherById(voucherId);
+
+      expect(mockWeb3.eth.Contract).toBeCalledTimes(1);
+    });
+
+    it('should throw an error if the contract method throws an error', async () => {
+      mockWeb3.eth.Contract = jest.fn().mockImplementation(() => {
+        return {
+          methods: {
+            vouchers: jest.fn().mockReturnValue({
+              call: jest.fn().mockRejectedValue(new Error('Contract Error')),
+            }),
+          },
+        };
+      });
+
+      smartContractService = new SmartContractService(
+        mockAppConfigService,
+        mockWeb3,
+      );
+
+      const logErrorSpy = jest.spyOn(helpers, 'logError');
+
+      await smartContractService.getVoucherById(voucherId);
+
+      expect(logErrorSpy).toHaveBeenCalledWith(
+        'Error in getVoucher: Error: Contract Error',
+      );
+
+      logErrorSpy.mockRestore();
     });
   });
 });
