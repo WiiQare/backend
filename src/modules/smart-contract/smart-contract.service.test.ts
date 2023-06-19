c66d813e28bb2810e9d1a10a7daa0183c3fe6274
lukebrichey <lukebrichey@gmail.com>
2023-06-19 11:39:50 -0400

    fix: Refactored unit test setup with proper mocks, added mintVoucher unit test

--- src/modules/smart-contract/smart-contract.service.test.ts
+++ src/modules/smart-contract/smart-contract.service.test.ts
@@ -1,61 +1,58 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { SmartContractService } from './smart-contract.service';
-import { TransactionService } from './transaction.service';
-import { nodeProvider } from './smart-contract.providers';
-import { getRepositoryToken } from '@nestjs/typeorm';
-import { Transaction } from './entities/transaction.entity';
 import { AppConfigService } from '../../config/app-config.service';
 import Web3 from 'web3';
-import { Contract } from 'web3-eth-contract';
-import { Account } from 'web3-core';
-
+import { MintVoucherDto } from './dto/mint-voucher.dto';
+import { VoucherStatus } from '../../common/constants/enums';
 describe('SmartContractService', () => {
+  // Mock services
+  const mockAppConfigService = {
+    smartContractPrivateKey:
+      '0x0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef',
+    smartContractAddress: '0xabcdef0123456789abcdef0123456789abcdef01234567',
+  } as unknown as AppConfigService;
+
+  const mockWeb3 = {
+    eth: {
+      getGasPrice: jest.fn(),
+      Contract: jest.fn().mockReturnValue({
+        methods: {
+          mintVoucher: jest.fn().mockReturnValue({
+            send: jest.fn().mockResolvedValue({
+              transactionHash: '0x123',
+            }),
+          }),
+        },
+      }),
+      accounts: {
+        privateKeyToAccount: jest.fn().mockReturnValue({
+          address: '0x1234567890abcdef1234567890abcdef12345678',
+        }),
+        wallet: {
+          add: jest.fn(),
+        },
+      },
+    },
+    Contract: jest.fn().mockReturnValue({
+      methods: {
+        mintVoucher: jest.fn().mockReturnValue({
+          send: jest.fn().mockResolvedValue({
+            transactionHash: '0x123',
+          }),
+        }),
+      },
+    }),
+  } as unknown as Web3;
+
   let smartContractService: SmartContractService;
-  let transactionService: TransactionService;
-  let appConfigService: AppConfigService;
-  let web3Mock: Web3;
-  let mockWiiqareContract: Contract;
-  let mockWiiQareAccount: Account;
 
   beforeEach(async () => {
-    const nodeProviderMock = {
-      provide: 'WEB3',
-      useFactory: () => {
-        return {
-          eth: {
-            getGasPrice: jest.fn(),
-            Contract: jest.fn(),
-            accounts: {
-              privateKeyToAccount: jest.fn().mockReturnValue({}),
-            },
-          },
-        };
-      },
-    } as unknown as typeof nodeProvider;
+    jest.clearAllMocks();
 
-    const moduleRef: TestingModule = await Test.createTestingModule({
-      providers: [
-        SmartContractService,
-        TransactionService,
-        {
-          provide: AppConfigService,
-          useValue: {},
-        },
-        nodeProviderMock,
-        {
-          provide: getRepositoryToken(Transaction),
-          useValue: {},
-        },
-      ],
-    }).compile();
-
-    smartContractService =
-      moduleRef.get<SmartContractService>(SmartContractService);
-    transactionService = moduleRef.get<TransactionService>(TransactionService);
-    appConfigService = moduleRef.get<AppConfigService>(AppConfigService);
-
-    mockWiiqareContract = {} as Contract;
-    mockWiiQareAccount = {} as Account;
+    // Initialize smartContractService
+    smartContractService = new SmartContractService(
+      mockAppConfigService,
+      mockWeb3,
+    );
   });
 
   describe('getGasFees', () => {
@@ -69,6 +66,28 @@ describe('SmartContractService', () => {
       );
       const result = await smartContractService.getGasFees();
       expect(result).toEqual(gasFees);
+    });
+  });
+
+  describe('mintVoucher', () => {
+    it('should mint a voucher', async () => {
+      const mintVoucherDto: MintVoucherDto = {
+        amount: 1,
+        currency: 'USD',
+        ownerId: '0x123',
+        patientId: '0x456',
+      };
+
+      const gasFees = { gasPrice: 1, safeLow: 1, standard: 1, fast: 1 };
+
+      smartContractService.getGasFees = jest.fn().mockResolvedValue(gasFees);
+
+      const response = await smartContractService.mintVoucher(mintVoucherDto);
+
+      expect(smartContractService.getGasFees).toBeCalledTimes(1);
+      expect(response).toEqual({
+        transactionHash: '0x123',
+      });
     });
   });
 });
