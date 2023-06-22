87f9ea13bb2e8fab98aa0f88c652dea0fb562f96
lukebrichey <lukebrichey@gmail.com>
2023-06-22 13:21:08 -0400

    test: Finished unit tests for retrievePayerAccountInfo method

--- src/modules/payer-svc/payer-svc.controller.test.ts
+++ src/modules/payer-svc/payer-svc.controller.test.ts
@@ -1,12 +1,16 @@
 import { Repository } from 'typeorm';
 import { PayerSvcController } from './payer-svc.controller';
 import { User } from '../session/entities/user.entity';
+import { Payer } from '../payer-svc/entities/payer.entity';
 import { PayerService } from './payer.service';
 import { CachingService } from '../caching/caching.service';
 import { SessionService } from '../session/session.service';
 import { PatientSvcService } from '../patient-svc/patient-svc.service';
 import { SearchPatientDto } from './dto/payer.dto';
 import { PatientResponseDto } from '../patient-svc/dto/patient.dto';
+import { UserRole, UserStatus } from '../../common/constants/enums';
+import { NotFoundException } from '@nestjs/common';
+import { _404 } from '../../common/constants/errors';
 
 describe('PayerSvcController', () => {
   let mockUserRepository: Partial<Repository<User>>;
@@ -24,13 +28,43 @@ describe('PayerSvcController', () => {
     email: 'email',
   };
 
+  const mockUUID = '66a3b552-5b0b-428d-bd4e-d9f2d8182ba3';
+
+  const mockUser: User = {
+    id: 'id',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    email: 'email',
+    phoneNumber: 'phoneNumber',
+    username: 'username',
+    role: UserRole.PAYER,
+    status: UserStatus.ACTIVE,
+    password: 'password',
+  };
+
+  const mockPayer: Payer = {
+    id: 'id',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    firstName: 'Jane',
+    lastName: 'Doe',
+    user: mockUser,
+    country: 'country',
+    homeAddress: 'homeAddress',
+    province: 'province',
+    city: 'city',
+    referralCode: 'referralCode',
+  };
+
   beforeEach(async () => {
     jest.clearAllMocks;
 
     // Mock dependencies
     mockUserRepository = {};
 
-    mockPayerService = {};
+    mockPayerService = {
+      findPayerById: jest.fn().mockResolvedValue(mockPayer),
+    };
 
     mockCachingService = {};
 
@@ -61,7 +95,7 @@ describe('PayerSvcController', () => {
   describe('retrievePatientByPhoneNumber', () => {
     const mockSearchPatientDto: SearchPatientDto = {
       phoneNumber: 'phoneNumber',
-      payerId: 'payerId',
+      payerId: mockUUID,
     };
 
     it('should retrieve a patient by phone number', async () => {
@@ -100,7 +134,26 @@ describe('PayerSvcController', () => {
     });
   });
 
-  describe('retrievePayerAccountInfo', () => {});
+  describe('retrievePayerAccountInfo', () => {
+    it('should retrieve the payer by id', async () => {
+      // Call method
+      const response = await payerSvcController.retrievePayerAccountInfo(
+        mockUUID,
+      );
+
+      expect(mockPayerService.findPayerById).toHaveBeenCalledWith(mockUUID);
+      expect(response).toEqual(mockPayer);
+    });
+
+    it('should throw an error if payer is not found', async () => {
+      mockPayerService.findPayerById = jest.fn().mockResolvedValue(null);
+
+      // Call method
+      expect(async () =>
+        payerSvcController.retrievePayerAccountInfo(mockUUID),
+      ).rejects.toThrow(new NotFoundException(_404.PAYER_NOT_FOUND));
+    });
+  });
 
   describe('createPayerAccount', () => {});
 
